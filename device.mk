64fe9c8f80c3729ff5de449c42569ee8485c20fa
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    sweet: switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Bug: 69270928
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- device.mk
+++ device.mk
@@ -284,8 +284,15 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.sweet-libperfmgr \
     android.hardware.power@1.2.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Properties
 include $(LOCAL_PATH)/properties.mk
