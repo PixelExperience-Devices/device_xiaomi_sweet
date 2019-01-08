d4f7f6667bf048cb5d67b805c0d8a60852575be9
dianlujitao <dianlujitao@lineageos.org>
2019-01-08 23:24:03 +0800

    sweet: Build libsensorndkbridge from source
    Change-Id: I9b00ac56b98314350c36523e02b5898193d89c0b

--- device.mk
+++ device.mk
@@ -102,6 +102,9 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor
 
 # GPS
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
