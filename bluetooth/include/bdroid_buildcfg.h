6549b7dab6f98dfcd6bccad23109f61949901dc0
Jake Weinstein <jake@aospa.co>
2020-04-27 05:19:43 +0200

    sweet: bt: Use device name as BT device name.
     * QCOM-BTD is an ugly string and the device name
       makes much more sense.
    
    Change-Id: I9f402b46107312c0720b5a43f1a56ba73543dce4

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -19,7 +19,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
+//#define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
