9c8f29e4116c605b400e3854bb549e552a8debed
daniml3 <daniel@danielml.dev>
2021-04-27 10:20:20 +0200

    sweet: lights: turn into an AIDL service
    * Based on AOSP's default lights AIDL implementation
    * Remove unused functions
    * Remove unused lights
    * Refactor specific lights handling
    
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I92a55e9a0dd9a5dcebc7ec1653dc7aca6126b34d

--- device.mk
+++ device.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.sweet
+    android.hardware.lights-service.sweet
 
 # Media
 PRODUCT_PACKAGES += \
