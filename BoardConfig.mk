60b2b20ebb38586238df4ae18759d89fd41c1024
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    sweet: BoardConfig: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += kpti=off
 
 #BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 earlycon=msm_geni_serial,0x880000
 
