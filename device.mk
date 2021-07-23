d3c715e69c833afca90d24839b05218c50b6e5f2
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-24 01:24:54 +0600

    sweet: Copy multitouch.jazzhand permissions to vendor
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I2c7c64605dee2f43a999a027cb34645e8713f914

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
