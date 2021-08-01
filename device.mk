6aaf9c6f479cb65d37390a9395099af4f65997f1
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-01 07:08:51 +0600

    sweet: overlay: Import device-specific power profile
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I131eab849af51d19fef8b7ca0c43a398b06d7d2e

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
