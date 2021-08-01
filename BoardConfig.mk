17f2f5e85f3c8d9c633c7bb303b6b169d5aeed7d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-01 07:00:44 +0600

    sweet: libinit: Add libinit for variants handling
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Ife93ff1c06bac4b9a07b2ebee2b7b4a0a1fea3f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,10 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 # Broken
 BUILD_BROKEN_DUP_RULES := true
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
+TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
+
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CONFIG := sweet_user_defconfig
---
+++ libinit/init_sweet.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_sm6150.h>
+
+static const variant_info_t sweet_global_info = {
+    .hwc_value = "GLOBAL",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "sweet",
+    .marketname = "Redmi Note 10 Pro",
+    .model = "M2101K6G",
+
+    .nfc = true,
+};
+
+static const variant_info_t sweet_eea_info = {
+    .hwc_value = "EEA",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "sweet",
+    .marketname = "Redmi Note 10 Pro",
+    .model = "M2101K6G",
+
+    .nfc = true,
+};
+
+static const variant_info_t sweetin_info = {
+    .hwc_value = "INDIA",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "sweetin",
+    .marketname = "Redmi Note 10 Pro Max",
+    .model = "M2101K6I",
+
+    .nfc = false,
+};
+
+static const std::vector<variant_info_t> variants = {
+    sweet_global_info,
+    sweet_eea_info,
+    sweetin_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+    set_dalvik_heap();
+}
