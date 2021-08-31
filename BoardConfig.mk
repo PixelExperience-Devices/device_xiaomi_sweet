d0334b2406aa265324a142d70f87e3f81606c49d
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-08-31 21:16:39 +0530

    sweet: BoardConfig: add kernel flags
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,25 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=1 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+
+#BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 earlycon=msm_geni_serial,0x880000
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := vendor/sweet_user_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
+
 # inherit from the proprietary version
 -include vendor/xiaomi/sweet/BoardConfigVendor.mk
