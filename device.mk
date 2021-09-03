eed8c97a6a847d0009a1f390d341ed2494fb500d
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-09-03 15:46:06 +0530

    sweet: rootdir: import rootdir files from LA.UM.9.1.r1-11100-SMxxx0.0
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -21,10 +21,19 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.dcvs.sh \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 # Don't build super.img.
 PRODUCT_BUILD_SUPER_PARTITION := false
