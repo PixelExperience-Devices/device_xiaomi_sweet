5a1a3e5ef66f9b55f3e0f5261b4331870cf515b7
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-09-12 22:13:47 +0530

    sweet: import sec_config
     - from LA.UM.9.1.r1-10900-SMxxx0.0
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
