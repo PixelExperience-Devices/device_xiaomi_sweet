cf23d56d117ebfa8f1e7d804a529e9ccdb036eee
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-09-15 18:26:34 +0530

    sweet: build drm clearkey
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3-service.clearkey
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
