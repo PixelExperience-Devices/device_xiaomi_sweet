98328f82848ef04a585075c1c855695dd3e28ef5
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-09-24 13:30:26 +0530

    sweet: import audio configs and build libs
     - prebuilt audio HAL
     - configs from sweetin-user 11 RKQ1.200826.002 V12.5.8.0.RKFINXM release-keys
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,34 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@6.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libtinycompress.vendor \
+    libvolumelistener
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
