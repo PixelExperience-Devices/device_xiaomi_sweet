b78cfc8a14c4eb95f0aed6644cac810e2f918200
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-09-25 18:58:15 +0530

    sweet: add perf configs
     - from sweetin-user 11 RKQ1.200826.002 V12.5.8.0.RKFINXM release-keys
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -197,6 +197,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/perf/,$(TARGET_COPY_OUT_VENDOR)/etc/perf)
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
