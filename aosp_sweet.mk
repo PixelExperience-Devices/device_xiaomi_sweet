a983f846a681cb54cf03b2704998bfb65c0a1a0c
jhenrique09 <jhsv09@gmail.com>
2022-01-05 11:57:37 -0300

    sweet: Enable quick tap
    Change-Id: I86135ea3c4fb9063f5158f4f7ddd2d7e0f6b8062
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common PixelExperience stuff
+TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_sweet
