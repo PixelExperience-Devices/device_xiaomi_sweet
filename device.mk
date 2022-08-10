8c02af2409c54a1186e0722d6430d1f3cda5e506
Pulkit077 <pulkitagarwal2k1@gmail.com>
2022-08-10 10:15:32 +0530

    sweet: import XiaomiParts from raphael
     - kanged Doze, DC Dimming & Thermal settings from https://github.com/TheScarastic/android_device_xiaomi_raphael/commits/e7db7fc560787440784b6c9787da2e1ab2695f59/parts
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
