0db3e9ca11878e1d383221c34f0f7a018188d880
TheScarastic <warabhishek@gmail.com>
2022-08-11 13:47:36 +0530

    sweet: sepolicy: add XiaomiParts to its own domain
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,6 +148,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 TARGET_SEPOLICY_DIR := msmsteppe
 -include device/qcom/sepolicy_vndr/SEPolicy.mk
 
