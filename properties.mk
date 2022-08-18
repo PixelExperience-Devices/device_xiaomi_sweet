81191fe62a55798e081892d5ae07a2a9347306a4
SamarV-121 <samarvispute121@pm.me>
2022-08-18 19:56:40 +0530

    sweet: properties: Disable continuous transaction tracing on all build types
    Change-Id: I5b2d3ba03208925f40fd65d6615d027f6c7b3320

--- properties.mk
+++ properties.mk
@@ -211,6 +211,7 @@ ro.vendor.qti.va_aosp.support=1
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.disable_backpressure=1 \
 debug.sf.enable_hwc_vds=1 \
+debug.sf.enable_transaction_tracing=false \
 debug.sf.latch_unsignaled=1 \
 debug.sf.predict_hwc_composition_strategy=0 \
 persist.sys.sf.color_saturation=1.0 \
