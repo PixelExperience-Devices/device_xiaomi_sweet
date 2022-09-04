f0c1691561370d06b3c77f4b950872ac984835a8
Pulkit077 <pulkitagarwal2k1@gmail.com>
2022-09-04 14:03:45 +0530

    PixelExperience bringup
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
