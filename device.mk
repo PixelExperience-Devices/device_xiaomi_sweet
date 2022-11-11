af25bc9d49c5fe5aa2ad4b3a9b6485e22dac4878
Krzysztof Kosiński <krzysio@google.com>
2022-11-11 05:41:10 +0000

    sweet: Add prebuilt protobuf 3.9.1 to vendor partition.
    Needed to work around a Qualcomm HAL prebuilt.
    
    Bug: 203713560
    Bug: 258322017
    Bug: 254854421
    Test: Forrest
    Change-Id: I4eebc60f10fbea6a66127ed3e08441db32536f78

--- device.mk
+++ device.mk
@@ -306,6 +306,10 @@ PRODUCT_SOONG_NAMESPACES += \
 # Properties
 include $(LOCAL_PATH)/properties.mk
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # qdcm
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
