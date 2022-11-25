09d6e3d3a691571caa83e293694d9f08dfde7329
Pulkit077 <pulkitagarwal2k1@gmail.com>
2022-11-25 16:25:29 +0000

    sweet: parts: drop haptic feedback level adjustment
    Revert "sweet: parts: Vibrate when changing haptic feedback level"
    
    Revert "sweet: parts: Introduce haptic feedback level adjustment"
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,7 +26,6 @@ import android.util.Log;
 import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.utils.FileUtils;
 
@@ -46,7 +45,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
-        HapticUtils.restoreLevel(context);
         ThermalUtils.startService(context);
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
