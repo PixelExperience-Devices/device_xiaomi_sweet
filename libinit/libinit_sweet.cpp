327d847b13285a77af0917724a3889e956310086
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-01-17 22:25:10 +0530

    sweet: update blobs & fingerprint from sweetin V13.0.9.0.SKFINXM
     - from sweetin-user 12 SKQ1.210908.001 V13.0.9.0.SKFINXM release-keys
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- libinit/libinit_sweet.cpp
+++ libinit/libinit_sweet.cpp
@@ -17,8 +17,8 @@ static const variant_info_t sweetin_info = {
     .device = "sweetin",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6P",
-    .build_fingerprint = "Redmi/sweetin/sweetin:12/RKQ1.210614.002/V13.0.5.0.SKFINXM:user/release-keys",
-    .build_description = "sweetin-user 12 SKQ1.210908.001 V13.0.5.0.SKFINXM release-keys",
+    .build_fingerprint = "Redmi/sweetin/sweetin:12/RKQ1.210614.002/V13.0.9.0.SKFINXM:user/release-keys",
+    .build_description = "sweetin-user 12 SKQ1.210908.001 V13.0.9.0.SKFINXM release-keys",
 
     .nfc = false,
 };
@@ -31,8 +31,8 @@ static const variant_info_t sweetinpro_info = {
     .device = "sweetin",
     .marketname = "Redmi Note 10 Pro Max",
     .model = "M2101K6I",
-    .build_fingerprint = "Redmi/sweetinpro/sweetin:12/RKQ1.210614.002/V13.0.5.0.SKFINXM:user/release-keys",
-    .build_description = "sweetinpro-user 12 SKQ1.210908.001 V13.0.5.0.SKFINXM release-keys",
+    .build_fingerprint = "Redmi/sweetinpro/sweetin:12/RKQ1.210614.002/V13.0.9.0.SKFINXM:user/release-keys",
+    .build_description = "sweetinpro-user 12 SKQ1.210908.001 V13.0.9.0.SKFINXM release-keys",
 
     .nfc = false,
 };
