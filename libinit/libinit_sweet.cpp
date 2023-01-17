810f7ca44ad634c5509e1d8a924a58b230b2ccce
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-01-17 22:34:11 +0530

    sweet: update blobs & fingerprint from sweet V13.0.15.0.SKFMIXM
     - from sweet-user 12 SKQ1.210908.001 V13.0.15.0.SKFMIXM release-keys
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- libinit/libinit_sweet.cpp
+++ libinit/libinit_sweet.cpp
@@ -45,8 +45,8 @@ static const variant_info_t sweet_global_info = {
     .device = "sweet",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6G",
-    .build_fingerprint = "Redmi/sweet_global/sweet:12/RKQ1.210614.002/V13.0.8.0.SKFMIXM:user/release-keys",
-    .build_description = "sweet_global-user 12 SKQ1.210908.001 V13.0.8.0.SKFMIXM release-keys",
+    .build_fingerprint = "Redmi/sweet_global/sweet:12/RKQ1.210614.002/V13.0.15.0.SKFMIXM:user/release-keys",
+    .build_description = "sweet_global-user 12 SKQ1.210908.001 V13.0.15.0.SKFMIXM release-keys",
 
     .nfc = true,
 };
