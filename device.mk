d735010ba85ee556e008163d6db618ffabdb8b3b
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-07-14 15:20:25 +0530

    Revert "sweet: add support for Mi Dolby"
    This reverts commit c2a53099d1f5a21cf0c058d5d09cd30a252f7f0b.
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -433,6 +433,3 @@ $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 
 # Inherit MIUI Camera
 $(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
-
-# Inherit MiSound
-$(call inherit-product-if-exists, vendor/xiaomi/sweet-misound/MiSound.mk)
