0d1d16d8037a6e898cc33b4038bef0cf23f23532
Edwin Moquete <edwinmmoquete@gmail.com>
2023-12-09 17:10:29 +0000

    sweet: properties: Disable frame rate override feature
    This is enabled by default on U, and it causes apps like
    Chrome and YouTube to render the frame rate at 30FPS when
    playing some videos.
    
    Change-Id: I649bf03d550c2b9726c7957d15ed09e455d874ec

--- properties.mk
+++ properties.mk
@@ -344,6 +344,7 @@ ro.build.shutdown_timeout=2
 # Surfaceflinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 debug.sf.frame_rate_multiple_threshold=120 \
+ro.surface_flinger.enable_frame_rate_override=false \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.has_HDR_display=true \
 ro.surface_flinger.has_wide_color_display=true \
